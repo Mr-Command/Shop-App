e2a20dd42a1a902e06ef7509a4625ba53be009a6
Mr-Command <96426891+Mr-Command@users.noreply.github.com>
2024-03-06 17:47:44 +0300

    Productlar düzenlendi.

--- src/Components/Product/product.js
+++ src/Components/Product/product.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import { AiFillStar } from "react-icons/ai";
-import foto1 from "../../image/image-1.png";
+import { IoIosStarHalf } from "react-icons/io";
+import puffchair from "../../image/puff_chair.jpg";
+import bambichair from "../../image/bambi_chair.jpg";
+import bambichair2 from "../../image/bambi_chair_2.jpg";
+import bambichair3 from "../../image/bambi_chair_3.jpg";
+import bambichair4 from "../../image/bambi_chair_4.webp";
+import officechair from "../../image/office_chair.jpg";
+import trestlebasechair from "../../image/trestle_base_chair.jpg";
+import upholsteredchair from "../../image/upholstered_chair.webp";
+import bambichair5 from "../../image/bambi_chair_5.webp";
+import woodchair from "../../image/wood_chair.jpg";
 import { CiStar } from "react-icons/ci";
 import "./product.css";
 
@@ -10,20 +20,138 @@ function Product() {
     return ( 
         <section className='card-container'>
             <section className='card'>
-                <img src={foto1} alt="Furniture" className='card-img'/>
+                <img src={puffchair} alt="Furniture" className='card-img'/>
                 <div className="card-details">
-                    <h3 className='card-title'>koltuk</h3>
+                    <h3 className='card-title'>Puff Chair</h3>
+                    <p className='new'>NEW!</p>
                     <section className='card-review'>
                     <AiFillStar className='rating-star'/>
                     <AiFillStar className='rating-star'/>
                     <AiFillStar className='rating-star'/>
+                    <CiStar className='rating-star'/>
+                    </section>
+                    <section className='card-price'>€287,00</section>
+                </div>
+            </section>
+            <section className='card'>
+                <img src={bambichair} alt="Furniture" className='card-img'/>
+                <div className="card-details">
+                    <h3 className='card-title'>Bambi Chair</h3>
+                    <section className='card-review'>
                     <AiFillStar className='rating-star'/>
-                    <CiStar />
+                    <AiFillStar className='rating-star'/>
+                    <IoIosStarHalf className='rating-star'/>
+                    <CiStar className='rating-star'/>
+                    </section>
+                    <section className='card-price'>€399,00</section>
+                </div>
+            </section>
+            <section className='card'>
+                <img src={woodchair} alt="Furniture" className='card-img'/>
+                <div className="card-details">
+                    <h3 className='card-title'>Wood Chair</h3>
+                    <section className='card-review'>
+                    <AiFillStar className='rating-star'/>
+                    <AiFillStar className='rating-star'/>
+                    <AiFillStar className='rating-star'/>
+                    <IoIosStarHalf className='rating-star'/>
+                    </section>
+                    <section className='card-price'>€198,00</section>
+                </div>
+            </section>
+            <section className='card'>
+                <img src={bambichair2} alt="Furniture" className='card-img'/>
+                <div className="card-details">
+                    <h3 className='card-title'>Bambi Chair</h3>
+                    <section className='card-review'>
+                    <AiFillStar className='rating-star'/>
+                    <AiFillStar className='rating-star'/>
+                    <AiFillStar className='rating-star'/>
+                    <IoIosStarHalf className='rating-star'/>
+                    </section>
+                    <section className='card-price'>€200,00</section>
+                </div>
+            </section>
+            <section className='card'>
+                <img src={bambichair3} alt="Furniture" className='card-img'/>
+                <div className="card-details">
+                    <h3 className='card-title'>Bambi Chair</h3>
+                    <section className='card-review'>
+                    <AiFillStar className='rating-star'/>
+                    <AiFillStar className='rating-star'/>
+                    <AiFillStar className='rating-star'/>
+                    <CiStar className='rating-star'/>
+                    </section>
+                    <section className='card-price'>€200,00</section>
+                </div>
+            </section>
+            <section className='card'>
+                <img src={officechair} alt="Furniture" className='card-img'/>
+                <div className="card-details">
+                    <h3 className='card-title'>Office Chair</h3>
+                    <section className='card-review'>
+                    <AiFillStar className='rating-star'/>
+                    <AiFillStar className='rating-star'/>
+                    <AiFillStar className='rating-star'/>
+                    <CiStar className='rating-star'/>
+                    </section>
+                    <section className='card-price'>€287,00</section>
+                </div>
+            </section>
+            <section className='card'>
+                <img src={upholsteredchair} alt="Furniture" className='card-img'/>
+                <div className="card-details">
+                    <h3 className='card-title'>Upholstered Chair</h3>
+                    <section className='card-review'>
+                    <AiFillStar className='rating-star'/>
+                    <AiFillStar className='rating-star'/>
+                    <IoIosStarHalf className='rating-star'/>
+                    <CiStar className='rating-star'/>
+                    </section>
+                    <section className='card-price'>€399,00</section>
+                </div>
+            </section>
+            <section className='card'>
+                <img src={trestlebasechair} alt="Furniture" className='card-img'/>
+                <div className="card-details">
+                    <h3 className='card-title'>Trestle-base Chair</h3>
+                    <section className='card-review'>
+                    <AiFillStar className='rating-star'/>
+                    <AiFillStar className='rating-star'/>
+                    <AiFillStar className='rating-star'/>
+                    <CiStar className='rating-star'/>
                     </section>
                     <section className='card-price'>€300</section>
                 </div>
             </section>
+            <section className='card'>
+                <img src={bambichair4} alt="Furniture" className='card-img'/>
+                <div className="card-details">
+                    <h3 className='card-title'>Bambi Chair</h3>
+                    <section className='card-review'>
+                    <AiFillStar className='rating-star'/>
+                    <AiFillStar className='rating-star'/>
+                    <AiFillStar className='rating-star'/>
+                    <IoIosStarHalf className='rating-star'/>
+                    </section>
+                    <section className='card-price'>€200,00</section>
+                </div>
+            </section>
+            <section className='card'>
+                <img src={bambichair5} alt="Furniture" className='card-img'/>
+                <div className="card-details">
+                    <h3 className='card-title'>Bambi Chair</h3>
+                    <section className='card-review'>
+                    <AiFillStar className='rating-star'/>
+                    <AiFillStar className='rating-star'/>
+                    <AiFillStar className='rating-star'/>
+                    <CiStar className='rating-star'/>
+                    </section>
+                    <section className='card-price'>€200,00</section>
+                </div>
+            </section>
         </section>
+        
     );
 }
 
