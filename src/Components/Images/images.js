20ec85c5b063fe091e2ad0e007e2d5ef67f5f429
Mr-Command <96426891+Mr-Command@users.noreply.github.com>
2024-03-11 11:21:13 +0300

    Responsive yapıldı

--- src/Components/Images/images.js
+++ src/Components/Images/images.js
@@ -1,33 +1,37 @@
 import React from 'react';
 import './images.css';
-import foto1 from "../../image/image.jpg"
+import foto1 from "../../image/Ligthning_pendant_lamps.png"
+import foto2 from "../../image/my_small_writing.png"
+import foto3 from "../../image/modular_longue.png"
+import foto4 from "../../image/modular_technical.png"
+import foto5 from "../../image/direct_ligth.png"
 
 function Images() {
     return ( 
         <div>
             <div>
-                <img className="big-Image" src={foto1} alt="" />
+                <img className="big-Image" src={foto1} alt="Büyük Resim" />
             </div>
             <div className='small-Images'>
             <img
-                src={foto1}
+                src={foto2}
                 alt="Küçük Resim"
                 className="small-Image-Left"
             />
              <div className="small-images-column">
                     <img
-                        src={foto1}
+                        src={foto3}
                         alt="Küçük Resim 2"
                         className="small-Image"
                     />
                     <img
-                        src={foto1}
+                        src={foto4}
                         alt="Küçük Resim 3"
                         className="small-Image"
                     />
                 </div>
                 <img
-                src={foto1}
+                src={foto5}
                 alt="Küçük Resim"
                 className="small-Image-Rigth"
             />
